6fd4f805e0e4d610d91d0dcdf82329118a0b731f
Afsar <mohammedafsar@propertyguru.com.sg>
2024-07-15 10:00:54 +0800

    Add Group by Status in the Table

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -16,14 +16,22 @@ $(document).ready(function () {
             // Add search functionality
             $("#search-deployments").on("keyup", function () {
                 var searchTerm = $(this).val().toLowerCase();
-                $("#deployments-table tbody tr").each(function () {
-                    var rowText = $(this).text().toLowerCase();
-                    $(this).toggle(rowText.includes(searchTerm));
+                $("#accordion .collapse.show").each(function () {
+                    var statusTable = $(this).find("table");
+                    var visibleRows = statusTable.find("tbody tr:visible");
+                    visibleRows.hide();
+
+                    statusTable.find("tbody tr").each(function () {
+                        var rowText = $(this).text().toLowerCase();
+                        if (rowText.includes(searchTerm)) {
+                            $(this).show();
+                        }
+                    });
                 });
 
-                // find the number of results
+                // Update filtered result count
                 var filteredResult = $(".results-found-text .result");
-                var resultCount = $("#deployments-table tbody tr:visible").length;
+                var resultCount = $(".card-body #accordion div.card-body tr:visible").length;
                 filteredResult.text(resultCount);
             });
 
@@ -31,8 +39,8 @@ $(document).ready(function () {
             $('#teamTabs a').on('click', function (e) {
                 e.preventDefault();
                 $(this).tab('show');
-                 var team = $(this).attr('id').split('-')[0]; // e.g., "core" from "core-tab"
-                if(team === 'internal'){
+                var team = $(this).attr('id').split('-')[0]; // e.g., "core" from "core-tab"
+                if (team === 'internal') {
                     team = 'internal tools';
                 }
                 var filteredData = team === 'all' ? sampleData : sampleData.filter(function (deployment) {
@@ -45,9 +53,9 @@ $(document).ready(function () {
         });
     });
 
-    $.getJSON("./data/data.json", function(data) {
+    $.getJSON("./data/data.json", function (data) {
         var date = new Date(data.upcomingGurulandDeploymentDate);
-        var options = {weekday: 'short', year: 'numeric', month: 'long', day: 'numeric'};
+        var options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' };
 
         // Guruland deployment date
         var glDate = date.toLocaleDateString('en-GB', options);
@@ -62,49 +70,145 @@ $(document).ready(function () {
 
     function getStatus(deployment) {
         if (!deployment.isReviewed) {
-            return '<span class="badge badge-warning">Pending Review</span>';
+            return 'Pending Review';
         } else if (deployment.isReviewed && !deployment.isMerged) {
-            return '<span class="badge badge-info">Pending Merge/QA</span>';
+            return 'Pending Merge/QA';
         } else if (deployment.isReviewed && deployment.isMerged && !deployment.isDeployed) {
-            return '<span class="badge badge-primary">Pending Deployment</span>';
+            return 'Pending Deployment';
         } else if (deployment.isReviewed && deployment.isMerged && deployment.isDeployed) {
-            return '<span class="badge badge-success">Deployed</span>';
+            return 'Deployed';
         }
         return '';
     }
 
     function populateDeploymentsTable(filteredData) {
-        var tbody = $("#upcoming-deployments");
-        tbody.empty();
+        var tbodyPendingReview = $("#status-pending-review tbody");
+        var tbodyPendingMergeQA = $("#status-pending-merge-qa tbody");
+        var tbodyPendingDeployment = $("#status-pending-deployment tbody");
+        var tbodyDeployed = $("#status-deployed tbody");
+
+        tbodyPendingReview.empty();
+        tbodyPendingMergeQA.empty();
+        tbodyPendingDeployment.empty();
+        tbodyDeployed.empty();
 
         var filteredResult = $(".results-found-text .result");
         filteredResult.text(filteredData.length);
+
         $.each(filteredData, function (index, deployment) {
             var prLinks = deployment.pull_requests.map(function (pr) {
                 return `<a href="${pr.url}" target="_blank">${pr.repositoryName.replace("project-", "").replace('pg-', '')}
-<!--if merged then success else warning-->
     <span class="badge badge-${pr.status === 'MERGED' ? 'success' : 'warning'}">${pr.status}</span>
-<!--    If status is deployed then primary -->
     <span class="badge badge-${pr.isDeployed ? 'primary' : ''} ">${pr.isDeployed ? 'Deployed' : ''}</span>
-       
 </a>`;
             }).join("<br>");
-            var jiraLink = `https://propertyguru.atlassian.net/browse/${deployment.key}`
+            var jiraLink = `https://propertyguru.atlassian.net/browse/${deployment.key}`;
 
             var row = `
                 <tr>
-                    <!--  Add Link to jira key-->
                     <td class="jira-id"><a href="${jiraLink}" target="_blank">${deployment.key}</a></td>
                     <td>${deployment.jira_title || ''}</td>
                     <td>${deployment.dev_name}</td>
-                    <td>
-                       ${getStatus(deployment)}
-                    </td>
                     <td class="pr-link">${prLinks}</td>
                 </tr>
             `;
-            tbody.append(row);
+
+            switch (getStatus(deployment)) {
+                case 'Pending Review':
+                    tbodyPendingReview.append(row);
+                    break;
+                case 'Pending Merge/QA':
+                    tbodyPendingMergeQA.append(row);
+                    break;
+                case 'Pending Deployment':
+                    tbodyPendingDeployment.append(row);
+                    break;
+                case 'Deployed':
+                    tbodyDeployed.append(row);
+                    break;
+            }
+            updateGroupCount();
         });
+
+        function updateGroupCount() {
+            var groupCount = $(".group-count");
+            var pendingReviewCount = tbodyPendingReview.find("tr").length;
+            var pendingMergeQACount = tbodyPendingMergeQA.find("tr").length;
+            var pendingDeploymentCount = tbodyPendingDeployment.find("tr").length;
+            var deployedCount = tbodyDeployed.find("tr").length;
+
+            groupCount.eq(0).text(pendingReviewCount);
+            groupCount.eq(1).text(pendingMergeQACount);
+            groupCount.eq(2).text(pendingDeploymentCount);
+            groupCount.eq(3).text(deployedCount);
+        }
+    }
+
+    function populateSearchResults(searchTerm) {
+        var searchResults = sampleData.filter(function (deployment) {
+            return deployment.jira_title.toLowerCase().includes(searchTerm);
+        });
+        //populateDeploymentsTable(searchResults);
+        // Headers - Jira Id, Jira Title, Team Name,  Dev Name, Status, PR Link
+        // Rows - Jira Id, Jira Title, Team Name,  Dev Name, Status, PR Link
+
+        // create new Table under `.card-body #accordion` with id `search-results`
+        // create new Table Header with id `search-results-header`
+        // create new Table Body with id `search-results-body`
+
+        $("#accordion").append(`<div class="card">
+            <div class="card-header" id="search-results-header">
+                <h5 class="mb-0">
+                    <button class="btn btn-link" data-toggle="collapse" data-target="#search-results" aria-expanded="true" aria-controls="search-results">
+                        Search Results
+                    </button>
+                </h5>
+            </div>
+            <div id="search-results" class="collapse show" aria-labelledby="search-results-header" data-parent="#accordion">
+                <div class="card-body">
+                    <table class="table table-striped" id="search-results-table">
+                        <thead>
+                            <tr>
+                                <th>Jira Id</th>
+                                <th>Jira Title</th>
+                                <th>Team Name</th>
+                                <th>Dev Name</th>
+                                <th>Status</th>
+                                <th>PR Link</th>
+                            </tr>
+                        </thead>
+                        <tbody id="search-results-body">
+                        </tbody>
+                    </table>
+                </div>
+            </div> 
+         </div>`);
+
+    }
+
+    function populateSearchResultsTable(searchResults) {
+        var tbody = $("#search-results-body");
+        tbody.empty();
+        $.each(searchResults, function (index, deployment) {
+            var prLinks = deployment.pull_requests.map(function (pr) {
+                return `<a href="${pr.url}" target="_blank">${pr.repositoryName.replace("project-", "").replace('pg-', '')}`;
+            }
+            ).join("<br>");
+            var jiraLink = `https://propertyguru.atlassian.net/browse/${deployment.key}`;
+            var row = `
+                <tr>
+                    <td class="jira-id"><a href="${jiraLink}" target="_blank">${deployment.key}</a></td>
+                    <td>${deployment.jira_title || ''}</td>
+                    <td>${deployment.team}</td>
+                    <td>${deployment.dev_name}</td>
+                    <td>${getStatus(deployment)}</td>
+                    <td class="pr-link">${prLinks}</td>
+                </tr>
+            `;
+
+            tbody.append(row);
+        }
+        );
     }
 
     function sortTable(sortField) {
